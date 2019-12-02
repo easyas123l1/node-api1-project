989a20a1fa6f5f1225c41b82004213f9b6f6a89a
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-02 15:03:59 -0600

    add users

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+  res.send({ api: 'ALIVE!' })
+})
+
+// create a user
+server.post('/api/users', (req, res) => {
+  const data = req.body;
+  if (!data.name || !data.bio) {
+    res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' })
+  }
+  db.add(data)
+  .then(user => {
+    res.status(201).json(user);
+  })
+  .catch(error => {
+    console.log('error on POST /users', error);
+    res.status(500).json({ errorMessage: 'error getting list of users from database' })
+  })
+})
+
+const port = 4000;
+server.listen(port, () => console.log(`\n ** API running on port ${port} ** \n `))
